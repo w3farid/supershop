3ffa624cd6e7555c4434de08e20cde1af4d2103a
farid <farid@tgalimited.com>
2018-08-10 21:46:27 +0600

    check login

--- app/src/main/java/com/supershop/service/UserService.java
+++ app/src/main/java/com/supershop/service/UserService.java
@@ -1,7 +1,11 @@
 package com.supershop.service;
 
+import android.content.Context;
+import android.text.TextUtils;
+
 import com.supershop.database.IUserSource;
 import com.supershop.model.User;
+import com.supershop.utils.SaveSharedPreference;
 
 import java.util.List;
 
@@ -29,12 +33,32 @@ public class UserService implements IUserService {
 
     @Override
     public List<User> getAll() {
+        String a = "ab";
         return null;
     }
 
     @Override
-    public boolean isLogin() {
+    public boolean isLogin(User user, Context context) {
+        boolean isLogin = validUsername(user.getUsername()) && validPassword(user.getPassword());
 
-        return false;
+        if (isLogin){
+            SaveSharedPreference.setLoggedIn(context, true);
+            return isLogin;
+        }else {
+            SaveSharedPreference.setLoggedIn(context, false);
+            return isLogin;
+        }
+
     }
+
+    private boolean validUsername(String username){
+        boolean isValid = !TextUtils.isEmpty(username);
+        return isValid;
+    }
+
+    private boolean validPassword(String password){
+        boolean isValid = !TextUtils.isEmpty(password);
+        return isValid;
+    }
+
 }
