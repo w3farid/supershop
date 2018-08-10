c3cba913ad911ba51d2c0d756bfc7c2f036acb0c
farid <farid@tgalimited.com>
2018-08-10 21:46:28 +0600

    check login

--- app/src/main/java/com/supershop/view/LoginActivity.java
+++ app/src/main/java/com/supershop/view/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.supershop.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -8,8 +9,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.supershop.R;
+import com.supershop.model.User;
 import com.supershop.service.IUserService;
+import com.supershop.service.UserService;
+import com.supershop.utils.SaveSharedPreference;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -18,19 +23,35 @@ public class LoginActivity extends Activity {
     @BindView(R.id.username)EditText username;
     @BindView(R.id.password)EditText password;
     @BindView(R.id.loginMessage)TextView loginMessage;
+    @BindString(R.string.loginErrorMessage) String loginErrorMessage;
 
-    IUserService userService;
+    User user;
+    private IUserService userService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        user = new User();
+        userService = new UserService();
+        if(SaveSharedPreference.getLoggedStatus(getApplicationContext())) {
+            Intent intent = new Intent(getApplicationContext(), dashboard.class);
+            startActivity(intent);
+        }
     }
 
     public void checkLogin(View view) {
         String uName = username.getText().toString();
         String pass = password.getText().toString();
-        Toast.makeText(this,uName+" and "+pass,Toast.LENGTH_LONG).show();
+        user.setUsername(uName);
+        user.setPassword(pass);
+        boolean isLogin = userService.isLogin(user, getApplicationContext());
+        if(isLogin){
+            Intent intent = new Intent(getApplicationContext(), dashboard.class);
+            startActivity(intent);
+        }else
+            loginMessage.setText(loginErrorMessage);
+
     }
 }
